d85d98ebee63cbf13eb8d111f1098da4e0895836
Prathmesh7076 <126493944+Prathmesh7076@users.noreply.github.com>
2025-07-13 22:28:21 +0530

    fix: wrap useSearchParams in Suspense properly

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 function SearchComponent() {
   const searchParams = useSearchParams();
